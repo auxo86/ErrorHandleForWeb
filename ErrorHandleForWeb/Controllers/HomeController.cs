d4846921a95b8b992eeab3080df4769e3fe2f7ad
auxo <auxo@mail.ndmctsgh.edu.tw>
2018-09-19 20:50:03 +0800

    顯示友善的錯誤葉面

---
+++ ErrorHandleForWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorHandleForWeb.Controllers
+{
+    //[HandleError]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //[HandleError]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HandleError]
+        public ActionResult ExceptionDemo()
+        {
+            int i = 0;
+            int j = 10 / i;
+            return View();
+        }
+    }
+}
